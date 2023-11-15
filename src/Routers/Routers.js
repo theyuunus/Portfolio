3973c5b905f9fe86395f95073777a6b4616ac678
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-15 19:43:23 +0500

    first commit

---
+++ src/Routers/Routers.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Projects from "../Pages/Projects";
+import Contact from "../Pages/Contact";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+  );
+};
