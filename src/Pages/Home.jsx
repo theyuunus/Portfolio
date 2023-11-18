bc1c66ab4def3f5ccd3b55bde9bb74adfcfd66e3
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-18 18:43:28 +0500

    #fix # add about of home #Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeHeader from '../components/HomeHeader/HomeHeader'
+import HomeAbout from '../components/HomeAbout/HomeAbout'
 
 export default function Home() {
   return (
     <div>
       <HomeHeader />
+      <HomeAbout />
     </div>
   )
 }
