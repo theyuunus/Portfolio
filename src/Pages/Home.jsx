8b6d3e9d2dae2959471e8921918a77a7e7f78829
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-18 16:20:21 +0500

    #fix #Add Home #Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomeHeader from '../components/HomeHeader/HomeHeader'
 
 export default function Home() {
   return (
     <div>
-
+      <HomeHeader />
     </div>
   )
 }
