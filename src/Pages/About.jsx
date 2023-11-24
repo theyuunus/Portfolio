646aeb87a1bbbcba765b885b78db97ad21a448ea
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-24 17:42:02 +0500

    #fix #add Skills of About #About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import AboutHeader from '../components/AboutHeader/AboutHeader'
+import AboutSkills from '../components/AboutSkills/AboutSkills'
 
 export default function About() {
   return (
     <Helmet title="About">
       <AboutHeader />
+      <AboutSkills />
     </Helmet>
   )
 }
