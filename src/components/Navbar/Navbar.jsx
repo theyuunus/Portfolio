e4191794ab16fc3fdcdb0e0b77149d377e997a3f
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-27 17:15:40 +0500

    #fix #add Navbar Burger open #Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import "./Navbar.scss"
-import Logo from "../../images/Logo.png"
-import { Link, NavLink } from 'react-router-dom'
+import React, { useRef, useState } from 'react';
+import "./Navbar.scss";
+import Logo from "../../images/Logo.png";
+import { Link, NavLink } from 'react-router-dom';
 import { FaGithub } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
 
 export default function Navbar() {
+  const [isMenuActive, setMenuActive] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    menuRef.current.classList.toggle("menu__active");
+    setMenuActive(!isMenuActive);
+  };
+
+  const closeMenu = () => {
+    if (menuRef.current.classList.contains("menu__active")) {
+      toggleMenu();
+    }
+  };
+
   return (
     <React.Fragment>
       <div>
@@ -31,14 +45,47 @@ export default function Navbar() {
               <NavLink to={"/contact"} className="navbar-right-a">
                 <span className="navbar-right-a-span">#</span>contacts
               </NavLink>
+              <span className="mobile__menu">
+                <i onClick={(e) => { e.stopPropagation(); toggleMenu(); }}>
+                  {isMenuActive ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <rect width="1" height="1" transform="matrix(1 0 0 -1 11 12)" fill="#D9D9D9" />
+                      <rect x="3" y="19.9706" width="24" height="2" transform="rotate(-45 3 19.9706)" fill="#D9D9D9" />
+                      <rect x="4" y="3" width="24" height="2" transform="rotate(45 4 3)" fill="#D9D9D9" />
+                    </svg>
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <rect width="24" height="2" fill="#D9D9D9" />
+                      <rect x="9" y="12" width="15" height="2" fill="#D9D9D9" />
+                    </svg>
+                  )}
+                </i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar_mobile">
+          <div className="navigation" ref={menuRef} onClick={closeMenu}>
+            <div className="menu">
+              <NavLink to={"/"} className="menu-a" onClick={closeMenu}>
+                <span className="menu-a-span">#</span>home
+              </NavLink>
+              <NavLink to={"works"} className="menu-a" onClick={closeMenu}>
+                <span className="menu-a-span">#</span>works
+              </NavLink>
+              <NavLink to={"/about"} className="menu-a" onClick={closeMenu}>
+                <span className="menu-a-span">#</span>about-me
+              </NavLink>
+              <NavLink to={"/contact"} className="menu-a" onClick={closeMenu}>
+                <span className="menu-a-span">#</span>contacts
+              </NavLink>
             </div>
           </div>
         </div>
 
         <div className="media">
-          <div className="media-top">
-
-          </div>
+          <div className="media-top"></div>
           <div className="media-bottom">
             <a className="media-bottom-a" href="https://github.com/theyuunus" target='_blank'>
               <FaGithub className="media-bottom-a-icon" />
@@ -50,5 +97,5 @@ export default function Navbar() {
         </div>
       </div>
     </React.Fragment>
-  )
+  );
 }
