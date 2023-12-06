9cea3531dd06735b2c0f0ecc5f2a1403ba37b176
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-06 19:06:43 +0500

    #fix #add small projects #Projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import Projects from '../components/Projects/Projects'
+import SmallProjects from '../components/SmallProjects/SmallProjects'
 
 export default function projects() {
   return (
     <Helmet title="Works">
       <Projects />
+      <SmallProjects />
     </Helmet>
   )
 }
