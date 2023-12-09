f2cbaaf7f31631b8c6cbe687fc5f3e9a2901c757
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-09 12:48:30 +0500

    #fix #add and fix card #SmallProjects

--- src/components/SmallProjects/SmallProjects.jsx
+++ src/components/SmallProjects/SmallProjects.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './SmallProjects.scss';
 import ButtonLive from '../ButtonLive/ButtonLive';
+import ButtonDemo from '../ButtonDemo/ButtonDemo'
 import axios from 'axios';
 
 export default function SmallProjects() {
@@ -29,20 +30,29 @@ export default function SmallProjects() {
                     <div className="smallProjects-cards">
                         {smallWork ? (
                             smallWork.map((work) => (
-                                <div className="smallProjects-cards-card" key={work.id}>
-                                    <div className="smallProjects-cards-card-top">
-                                        <p className="smallProjects-cards-card-top-p">
-                                            {work.languages}
-                                        </p>
-                                    </div>
-                                    <div className="smallProjects-cards-card-bottom">
-                                        <h1 className="smallProjects-cards-card-bottom-h1">
-                                            {work.title}
-                                        </h1>
-                                        <p className="smallProjects-cards-card-bottom-p">
-                                            {work.text}
-                                        </p>
-                                        <ButtonLive text={"Github <~>"} link={work.codes} />
+                                <div style={{ alignItems: "start" }}>
+                                    <div className="smallProjects-cards-card" key={work.id}>
+                                        <div className="smallProjects-cards-card-top">
+                                            <p className="smallProjects-cards-card-top-p">
+                                                {work.languages}
+                                            </p>
+                                        </div>
+                                        <div className="smallProjects-cards-card-bottom">
+                                            <h1 className="smallProjects-cards-card-bottom-h1">
+                                                {work.title}
+                                            </h1>
+                                            <p className="smallProjects-cards-card-bottom-p">
+                                                {work.text}
+                                            </p>
+                                            <div className="smallProjects-cards-card-buttons">
+                                                {work.live ? (
+                                                    <ButtonLive text={"Live <~>"} link={work.live} />
+                                                ) : null}
+                                                {work.codes ? (
+                                                    <ButtonDemo text={"Github <~>"} link={work.codes} />
+                                                ) : null}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             ))
