512599360d101a026b989f8ad9d95c81ee7a2525
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-14 11:54:09 +0500

    #fix #fix and change navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,28 +47,37 @@ export default function Navbar() {
               </NavLink>
               <span className="mobile__menu">
                 <i onClick={toggleMenu} role="button" aria-label="Toggle Menu">
-                  {isMenuActive ? (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                      <rect width="1" height="1" transform="matrix(1 0 0 -1 11 12)" fill="#D9D9D9" />
-                      <rect x="3" y="19.9706" width="24" height="2" transform="rotate(-45 3 19.9706)" fill="#D9D9D9" />
-                      <rect x="4" y="3" width="24" height="2" transform="rotate(45 4 3)" fill="#D9D9D9" />
-                    </svg>
-                  ) : (
-                    <svg className="mobile__menu-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                      <rect width="24" height="2" fill="#D9D9D9" />
-                      <rect x="9" y="12" width="15" height="2" fill="#D9D9D9" />
-                    </svg>
-             
+                  <svg className="mobile__menu-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <rect width="24" height="2" fill="#D9D9D9" />
+                    <rect x="9" y="12" width="15" height="2" fill="#D9D9D9" />
+                  </svg>
                 </i>
               </span>
             </div>
           </div>
         </div>
-
         <div className="navbar_mobile">
           <div className="navigation" ref={menuRef} onClick={closeMenu}>
             <div className="menu">
-
+              <div className="menu-close_svg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <rect width="1" height="1" transform="matrix(1 0 0 -1 11 12)" fill="#D9D9D9" />
+                  <rect x="3" y="19.9706" width="24" height="2" transform="rotate(-45 3 19.9706)" fill="#D9D9D9" />
+                  <rect x="4" y="3" width="24" height="2" transform="rotate(45 4 3)" fill="#D9D9D9" />
+                </svg>
+              </div>
+              <NavLink to={"/"} className="menu-a">
+                <span className="navbar-right-a-span">#</span>home
+              </NavLink>
+              <NavLink to={"works"} className="menu-a">
+                <span className="navbar-right-a-span">#</span>works
+              </NavLink>
+              <NavLink to={"/about"} className="menu-a">
+                <span className="navbar-right-a-span">#</span>about-me
+              </NavLink>
+              <NavLink to={"/contact"} className="menu-a">
+                <span className="navbar-right-a-span">#</span>contacts
+              </NavLink>
             </div>
           </div>
         </div>
